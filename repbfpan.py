e26bb59b1f996dbff5a0e020e50b7ee76024bef3
gdzengzeng <149512725+gdzengzeng@users.noreply.github.com>
2025-05-09 11:29:16 +0800

    repbfpan.py

--- repbfpan.py
+++ repbfpan.py
@@ -94,7 +94,7 @@ class RepVGGBlock(nn.Module):
     def get_equivalent_kernel_bias(self):
         kernel3x3, bias3x3 = self._fuse_bn_tensor(self.rbr_dense)
         kernel1x1, bias1x1 = self._fuse_bn_tensor(self.rbr_1x1)
-        kernelid, biasid = self._fuse_bn_tensor(self.rbr_identity)
+        kernelid, biasid = self._fuse_bn_tensor(self.rbr_identity) 
         return kernel3x3 + self._pad_1x1_to_3x3_tensor(kernel1x1) + kernelid, bias3x3 + bias1x1 + biasid
 
     def _pad_1x1_to_3x3_tensor(self, kernel1x1):
